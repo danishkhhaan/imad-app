fb5cf1c49f86fc832d264c021c546ad92efaef63
danishkhhaan <danish.khhaan@gmail.com>
2017-08-20 16:14:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@
 var button = document.getElementById('counter');
 
 button.onclick = function () {
+   
     //Create a request object
     var request = new XMLHttpRequest();
     
@@ -22,3 +23,23 @@ button.onclick = function () {
     request.open('GET', 'http://danishkhhaan.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  // Make a request to the server and send the name
+  // Capture a list of names and render as a list
+  var names = ['name1', 'name2', 'name3'];
+  var list = '';
+  for (var i=0; i<names.length; i++) {
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
+
+
+
+
